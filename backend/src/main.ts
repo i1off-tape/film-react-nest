06158dbe0c069f58dec321db8ed7cb3324bb9779
Vadim Ivanov <jhoncrendy18@gmail.com>
2026-05-06 19:59:20 +0300

    fix path, remove todo

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
-import { RequestMethod } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/afisha', {
-    exclude: [{ path: 'order', method: RequestMethod.POST }],
-  });
+  app.setGlobalPrefix('api/afisha');
   app.enableCors();
   await app.listen(3000);
 }
